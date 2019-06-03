9d85ae87e79d4b28e4439ffdd0b36ae55f3c6010
ArkadyKid <kidanov.a.y1986@gmail.com>
2019-06-03 17:59:32 +0300

    Поправил синтаксис js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-var popup = document.querySelector ('.modal');
-var openPopup = document.querySelector ('.contacts__button-link');
-var closePopup = popup.querySelector ('.close');
+var popup = document.querySelector('.modal');
+var openPopup = document.querySelector('.contacts__button-link');
+var closePopup = popup.querySelector('.close');
 var modalShow = 'modal--show';
 var popupMap = document.querySelector('.map');
 var openPopupMap = document.querySelector('.contacts-map__link');
 var closePopupMap = popupMap.querySelector('.close');
 var mapShow = 'map--show';
 
-openPopup.addEventListener ('click', function(evt) {
-  evt.preventDefault ();
-  popup.classList.add (modalShow);
+openPopup.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  popup.classList.add(modalShow);
 });
-closePopup.addEventListener ('click', function(evt) {
-  evt.preventDefault ();
-  popup.classList.remove (modalShow);
+closePopup.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  popup.classList.remove(modalShow);
 })
-document.addEventListener ('keydown', function(evt) {
-  if (evt.keyCode===27) {
-    popup.classList.remove (modalShow);
+document.addEventListener('keydown', function(evt) {
+  if (evt.keyCode===27){
+    popup.classList.remove(modalShow);
   }
 })
 
@@ -31,6 +31,6 @@ closePopupMap.addEventListener('click', function(evt) {
 })
 document.addEventListener('keydown', function(evt) {
   if (evt.keyCode===27){
-    popupMap.classList.remove(mapShow);
+    popupMap.classList.remove (mapShow);
   }
 })
