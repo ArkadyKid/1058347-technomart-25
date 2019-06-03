e223b25359994a32c294fdfff197a88e42abfe03
ArkadyKid <kidanov.a.y1986@gmail.com>
2019-06-03 17:56:57 +0300

    Добавил попап на главной

--- js/script.js
+++ js/script.js
@@ -1,18 +1,36 @@
-var popup = document.querySelector('.map');
-var openPopup = document.querySelector('.contacts-map__link');
-var closePopup = popup.querySelector('.close');
+var popup = document.querySelector ('.modal');
+var openPopup = document.querySelector ('.contacts__button-link');
+var closePopup = popup.querySelector ('.close');
+var modalShow = 'modal--show';
+var popupMap = document.querySelector('.map');
+var openPopupMap = document.querySelector('.contacts-map__link');
+var closePopupMap = popupMap.querySelector('.close');
 var mapShow = 'map--show';
 
-openPopup.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  popup.classList.add(mapShow);
+openPopup.addEventListener ('click', function(evt) {
+  evt.preventDefault ();
+  popup.classList.add (modalShow);
 });
-closePopup.addEventListener('click', function(evt) {
+closePopup.addEventListener ('click', function(evt) {
+  evt.preventDefault ();
+  popup.classList.remove (modalShow);
+})
+document.addEventListener ('keydown', function(evt) {
+  if (evt.keyCode===27) {
+    popup.classList.remove (modalShow);
+  }
+})
+
+openPopupMap.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  popupMap.classList.add(mapShow);
+});
+closePopupMap.addEventListener('click', function(evt) {
   evt.preventDefault();
   popup.classList.remove(mapShow);
 })
 document.addEventListener('keydown', function(evt) {
   if (evt.keyCode===27){
-    popup.classList.remove(mapShow);
+    popupMap.classList.remove(mapShow);
   }
 })
