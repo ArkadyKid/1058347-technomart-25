13a055209ad67af687c2a5d0320c80936d94bb67
ArkadyKid <kidanov.a.y1986@gmail.com>
2019-06-09 22:32:08 +0300

    Добавил счётчик добавления в корзину и в закладки

--- js/script-catalog.js
+++ js/script-catalog.js
@@ -2,16 +2,30 @@
 (function () {
   var overflowClass = 'overflow';
   var orderDialogClass = 'order-dialog--show';
+  var linkRedClass = 'customer-menu__link--red';
 
-  var popupElement = document.querySelector('.order-dialog');
-  var openButtonsElement = document.querySelectorAll('.card__price-button, .card__button--green');
+  var addBookmarksElement = document.querySelectorAll('.card__button--white');
   var bodyElement = document.querySelector('body');
   var closeElement = document.querySelectorAll('.close, .link-button__button--white, .overlay');
+  var linkBookmarksElement = document.querySelector('.customer-menu__link--bookmark');
+  var linkCartElement = document.querySelector('.customer-menu__link--cart');
+  var openButtonsElement = document.querySelectorAll('.card__price-button, .card__button--green');
+  var popupElement = document.querySelector('.order-dialog');
+  var valueBookmarksElement = document.querySelector('.customer-menu__value--bookmark');
+  var valueCartElement = document.querySelector('.customer-menu__value--cart');
+
+  var bookmarksValue = function(evt) {
+    evt.preventDefault();
+    linkBookmarksElement.classList.add(linkRedClass);
+    valueBookmarksElement.innerHTML ++;
+  };
 
   var openPopup = function(evt) {
     evt.preventDefault();
     popupElement.classList.add(orderDialogClass);
     bodyElement.classList.add(overflowClass);
+    linkCartElement.classList.add(linkRedClass);
+    valueCartElement.innerHTML ++;
   };
 
   var mouseClose = function(evt) {
@@ -29,6 +43,10 @@
 
   openButtonsElement.forEach(function(element) {
     element.addEventListener('click', openPopup);
+  });
+
+  addBookmarksElement.forEach(function(element) {
+    element.addEventListener ('click', bookmarksValue);
   });
 
   closeElement.forEach(function(element) {
